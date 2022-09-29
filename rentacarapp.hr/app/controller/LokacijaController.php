b7c9f4f2ba9fb1577e9add1acf128b2176e04d1f
majera96 <majer.antonio@gmail.com>
2022-09-29 13:29:59 +0200

    dodana lokacija

---
+++ rentacarapp.hr/app/controller/LokacijaController.php
@@ -0,0 +1,96 @@
+<?php
+
+class LokacijaController extends AutorizacijaController
+{
+
+    private $phtmlDir = 'privatno' . 
+        DIRECTORY_SEPARATOR . 'lokacije' .
+        DIRECTORY_SEPARATOR;
+
+    private $entitet=null;
+    private $poruka='';
+
+    public function index()
+    {
+        $this->view->render($this->phtmlDir . 'index',[
+            'entiteti'=>Lokacija::read()
+        ]);
+    }
+
+    public function nova()
+    {
+        $novaLokacija = Lokacija::create([
+            'naziv_ulice'=>'',
+            'broj_ulice'=>'',
+            'postanski_broj'=>'',
+            'grad'=>'',
+            'broj_mobitela'=>'',
+            'email'=>''
+        ]);
+        header('location: ' . App::config('url') 
+                . 'lokacija/promjena/' . $novaLokacija);
+    }
+    
+    public function promjena($sifra)
+    {
+        if(!isset($_POST['naziv_ulice'])){
+
+            $e = Lokacija::readOne($sifra);
+            if($e==null){
+                header('location: ' . App::config('url') . 'lokacija');
+            }
+
+            $this->view->render($this->phtmlDir . 'detalji',[
+                'e' => $e,
+                'poruka' => 'Unesite podatke'
+            ]);
+            return;
+        }
+
+        $this->entitet = (object) $_POST;
+        $this->entitet->sifra=$sifra;
+    
+        if($this->kontrola()){
+            Lokacija::update((array)$this->entitet);
+            header('location: ' . App::config('url') . 'lokacija');
+            return;
+        }
+
+        $this->view->render($this->phtmlDir . 'detalji',[
+            'e'=>$this->entitet,
+            'poruka'=>$this->poruka
+        ]);
+    }
+
+  /*  private function kontrola()
+    {
+        return $this->kontrolaIme() && $this->kontrolaPrezime()
+        && $this->kontrolaOib();
+    }
+
+    private function kontrolaIme()
+    {
+        if(strlen($this->entitet->ime)===0){
+            $this->poruka = 'Ime obavezno';
+            return false;
+        }
+        return true;
+    }
+
+    private function kontrolaPrezime()
+    {
+        if(strlen($this->entitet->prezime)===0){
+            $this->poruka = 'Prezime obavezno';
+            return false;
+        }
+        return true;
+    }
+*/
+
+    public function brisanje($sifra)
+    {
+        Lokacija::delete($sifra);
+        header('location: ' . App::config('url') . 'lokacija');
+    }
+
+}
---
+++ rentacarapp.hr/app/model/Lokacija.php
@@ -0,0 +1,79 @@
+<?php
+
+class Lokacija
+{
+
+    public static function readOne($sifra)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            select * from lokacija where sifra=:sifra
+        
+        ');
+        $izraz->execute([
+            'sifra'=>$sifra
+        ]);
+        return $izraz->fetch(); 
+    }
+
+    public static function read()
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            select * from lokacija order by grad
+        
+        ');
+        $izraz->execute();
+        return $izraz->fetchAll();
+    }
+
+    public static function create($lokacija)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            insert into 
+            lokacija(naziv_ulice,broj_ulice,postanski_broj,grad,broj_mobitela,email)
+            values (:naziv_ulice,:broj_ulice,:postanski_broj,:grad,:broj_mobitela,:email)
+        
+        ');
+        $izraz->execute($lokacija);
+    }
+
+    public static function update($lokacija)
+    {
+     
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            update lokacija set
+            naziv_ulice=:naziv_ulice,
+                broj_ulice=:broj_ulice,
+                postanski_broj=:postanski_broj,
+                grad=:grad,
+                broj_mobitela=:broj_mobitela,
+                email=:email
+                    where sifra=:sifra
+        
+        ');
+        $izraz->execute($lokacija);
+
+    }
+
+     public static function delete($sifra)
+     {
+         $veza = DB::getInstance();
+         $izraz = $veza->prepare('
+         
+            delete from lokacija where sifra=:sifra 
+         
+         ');
+         $izraz->execute([
+             'sifra'=>$sifra
+         ]);
+         
+     }
+ 
+ }
