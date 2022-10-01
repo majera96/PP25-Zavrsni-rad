f1bcc74209e047618dd9983c24588a74f48bfa9c
majera96 <majer.antonio@gmail.com>
2022-10-01 17:31:22 +0200

    dodan read i readOne rezervacija
    jos brisanje i promjena

---
+++ rentacarapp.hr/app/controller/RezervacijaController.php
@@ -0,0 +1,97 @@
+<?php
+
+class RezervacijaController extends AutorizacijaController
+{
+
+    private $phtmlDir = 'privatno' . 
+        DIRECTORY_SEPARATOR . 'rezervacije' .
+        DIRECTORY_SEPARATOR;
+
+    private $entitet=null;
+    private $poruka='';
+
+    public function index()
+    {
+        $nf = new NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
+        $this->view->render($this->phtmlDir . 'index',[
+            'entiteti'=>Rezervacija::read()
+        ]);
+    }
+
+    public function nova()
+    {
+        $novaRezervacija = Rezervacija::create([
+            'vozilo'=>'',
+            'cijena'=>'',
+            'lokacija'=>'',
+            'datum_preuzimanja'=>'',
+            'datum_povratka'=>'',
+            'korisnik'=>'',
+            'osiguranje'=>''
+        ]);
+        header('location: ' . App::config('url') 
+                . 'rezervacija/promjena/' . $novaRezervacija);
+    }
+    
+    public function promjena($sifra)
+    {
+        if(!isset($_POST['vozilo'])){
+
+            $e = Rezervacija::readOne($sifra);
+            if($e==null){
+                header('location: ' . App::config('url') . 'rezervacija');
+            }
+
+            $this->view->render($this->phtmlDir . 'detalji',[
+                'e' => $e,
+                'poruka' => 'Unesite podatke'
+            ]);
+            return;
+        }
+
+        $this->entitet = (object) $_POST;
+        $this->entitet->osiguranje = isset($_POST['osiguranje']);
+        $this->entitet->sifra=$sifra;
+    
+        if($this->kontrola()){
+            Rezervacija::update((array)$this->entitet);
+            header('location: ' . App::config('url') . 'rezervacija');
+            return;
+        }
+
+        $this->view->render($this->phtmlDir . 'detalji',[
+            'e'=>$this->entitet,
+            'poruka'=>$this->poruka
+        ]);
+
+    }
+
+    private function kontrola()
+    {
+        return $this->kontrolirajVozilo() && $this->kontrolirajLokaciju() && $this->kontrolirajOsiguranje();
+    }
+        
+    private function kontrolirajVozilo()
+    {
+        return true;
+    }
+
+    private function kontrolirajLokaciju()
+    {
+
+        return true;
+    }
+
+    private function kontrolirajOsiguranje()
+    {
+
+        return true;
+    }
+
+    public function brisanje($sifra)
+    {
+        Rezervacija::delete($sifra);
+        header('location: ' . App::config('url') . 'rezervacija');
+    }
+
+}
---
+++ rentacarapp.hr/app/model/Rezervacija.php
@@ -0,0 +1,80 @@
+<?php
+
+class Rezervacija
+{
+
+    public static function brisanje($sifra)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            delete from rezervacija where sifra=:sifra
+        
+        ');
+        $izraz->execute([
+            'sifra'=>$sifra
+        ]);
+        $izraz->fetchColumn();
+    }
+
+    public static function readOne($sifra)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        select a.sifra,e.ime,e.prezime,e.broj_vozacke,c.grad, c.naziv_ulice ,b.proizvodac ,b.model, a.cijena ,a.datum_preuzimanja,a.datum_povratka ,a.osiguranje 
+        from rezervacija a inner join vozilo b 
+        on a.vozilo = b.sifra
+        inner join lokacija c 
+        on a.lokacija = c.sifra 
+        inner join korisnik e 
+        on a.korisnik = e.sifra
+        where a.sifra=:sifra
+        
+        ');
+        $izraz->execute([
+            'sifra'=>$sifra
+        ]);
+        return $izraz->fetch(); 
+    }
+
+    // CRUD - R
+    public static function read()
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+        select a.sifra,e.ime,e.prezime,e.broj_vozacke,c.grad, c.naziv_ulice, c.broj_ulice 
+        ,b.proizvodac ,b.model, a.cijena ,a.datum_preuzimanja,a.datum_povratka ,a.osiguranje 
+        from rezervacija a inner join vozilo b 
+        on a.vozilo = b.sifra
+        inner join lokacija c 
+        on a.lokacija = c.sifra 
+        inner join korisnik e 
+        on a.korisnik = e.sifra
+        group by a.sifra,e.ime,e.prezime,e.broj_vozacke,c.grad, c.naziv_ulice ,b.proizvodac 
+        ,b.model, a.cijena ,a.datum_preuzimanja,a.datum_povratka ,a.osiguranje
+        order by 4,3
+        
+        ');
+        $izraz->execute(); 
+        return $izraz->fetchAll();
+    }
+
+    // CRUD - C
+    public static function create($p)
+{
+    // pokusavam skuziti
+}
+    // CRUD - U
+    public static function update($vozilo)
+    {
+// uskoro
+    }
+
+     // CRUD - D
+    public static function delete($sifra)
+    {
+//uskoro
+    }
+}
+
