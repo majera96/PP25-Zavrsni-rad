7df21e549b6d5113e1309baa2f2f6a38398c0b82
majera96 <majer.antonio@gmail.com>
2022-10-14 10:48:46 +0200

    Update VoziloController.php
    kontrole za vozila

--- rentacarapp.hr/app/controller/VoziloController.php
+++ rentacarapp.hr/app/controller/VoziloController.php
@@ -65,22 +65,66 @@ class VoziloController extends AutorizacijaController
 
     private function kontrola()
     {
-        return $this->kontrolirajProizvodac() && $this->kontrolirajGodiste() && $this->kontrolirajModel();
+        return $this->kontrolirajProizvodac() && $this->kontrolirajGodiste() && $this->kontrolirajModel() $$ $this->kontrolirajOpis();
     }
         
     private function kontrolirajProizvodac()
     {
+        $this->entitet->proizvodac = trim(str_replace(' ', '', (str_replace('&nbsp;', '', $this->entitet->proizvodac))));
+
+        if ($this->entitet->proizvodac == '') {
+            $this->poruka = 'Proizvođač vozila je obavezan';
+            return false;
+        }
         return true;
     }
 
     private function kontrolirajModel()
     {
+        $this->entitet->model = trim(str_replace(' ', '', (str_replace('&nbsp;', '', $this->entitet->model))));
+
+        if ($this->entitet->model == '') {
+            $this->poruka = 'Model vozila je obavezan';
+            return false;
+        }
 
         return true;
     }
 
     private function kontrolirajGodiste()
     {
+
+        $this->entitet->godiste = trim(str_replace(' ', '', (str_replace('&nbsp;', '', $this->entitet->godiste))));
+
+        if ($this->entitet->godiste == '') {
+            $this->poruka = 'Godište vozila je obavezno';
+            return false;
+        }
+
+        $godiste = (int)$this->entitet->godiste;
+
+        if($godiste<=2015){
+            $this->poruka = 'Godište vozila mora biti maksimalne starosti od 7 godina';
+            return false;
+        }
+
+        if($godiste>2023) {
+            $this->poruka = 'Živimo u 2022. godini';
+            return false;
+        }
+
+        return true;
+
+    }
+
+    private function kontrolirajOpis()
+    {
+        $this->entitet->opis=trim(str_replace('&nbsp;', '', $this->entiter->opis));
+
+        if(strlen(($this->entitet->opis))>100) {
+            $this->poruka = 'Maksimalno unjeti 100 znakova';
+            return false;
+        }
 
         return true;
     }
