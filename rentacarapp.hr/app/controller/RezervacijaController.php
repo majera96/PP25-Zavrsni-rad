e3b56942f8a7fe6eacd2dbbc635814c77625acb5
majera96 <majer.antonio@gmail.com>
2022-10-25 10:14:53 +0200

    pokusaj popravka

--- rentacarapp.hr/app/controller/RezervacijaController.php
+++ rentacarapp.hr/app/controller/RezervacijaController.php
@@ -35,6 +35,11 @@ class RezervacijaController extends AutorizacijaController
     
     public function promjena($sifra)
     {
+
+        $korisnici = $this->ucitajKorisnike();
+        $lokacije = $this->ucitajLokacije();
+        $vozila = $this->ucitajVozila();
+
         if(!isset($_POST['vozilo'])){
 
             $e = Rezervacija::readOne($sifra);
@@ -42,31 +47,33 @@ class RezervacijaController extends AutorizacijaController
                 header('location: ' . App::config('url') . 'rezervacija');
             }
 
-            $this->view->render($this->phtmlDir . 'detalji',[
-                'e' => $e,
-                'poruka' => 'Unesite podatke'
-            ]);
+            $this->detalji($e,$korisnici,$lokacije,$vozila,'Unesite podatke');
             return;
         }
 
         $this->entitet = (object) $_POST;
         $this->entitet->osiguranje = isset($_POST['osiguranje']);
         $this->entitet->sifra=$sifra;
-    
+
         if($this->kontrola()){
             Rezervacija::update((array)$this->entitet);
             header('location: ' . App::config('url') . 'rezervacija');
             return;
         }
 
-        $this->view->render($this->phtmlDir . 'detalji',[
-            'e'=>$this->entitet,
-            'poruka'=>$this->poruka
-        ]);
-
+        $this->detalji($this->entitet,$korisnici,$lokacije,$vozila,$this->poruka);
     }
 
-    
+    private function detalji($e,$korisnici,$lokacije,$vozila,$poruka)
+    {
+        $this->view->render($this->phtmlDir . 'detalji', [
+            'e'=>$e,
+            'lokacije'=>$lokacije,
+            'vozila'=>$vozila,
+            'korisnici'=>$korisnici,
+            'poruka'=>$poruka
+        ]);
+    }
 
     private function kontrola()
     {
@@ -89,6 +96,49 @@ class RezervacijaController extends AutorizacijaController
 
         return true;
     }
+
+    private function ucitajVozila()
+    {
+        $vozila = [];
+        $v = new stdClass();
+        $v->sifra = 0;
+        $v->proizvodac = 'Odaberi';
+        $v->model = 'Vozilo';
+        $vozila[] = $v;
+        foreach(Vozilo::read() as $vozilo){
+            $vozila[]=$vozilo;
+        }
+        return $vozila;
+    }
+
+    private function ucitajKorisnike()
+    {
+        $korisnici = [];
+        $k = new stdClass();
+        $k->sifra = 0;
+        $k->ime = 'Odaberi';
+        $k->prezime = 'korisnika';
+        $korisnici[] = $k;
+        foreach(Korisnik::read() as $korisnik){
+            $korisnici[]=$korisnik;
+        }
+        return $korisnici;
+    }
+
+    private function ucitajLokacije()
+    {
+        $lokacije = [];
+        $l = new stdClass();
+        $l->sifra = 0;
+        $l->grad = 'Odaberi';
+        $l->naziv_ulice = 'poslovnicu';
+        $lokacije[] = $l;
+        foreach(Lokacija::read() as $lokacija){
+            $lokacije[]=$lokacija;
+        }
+        return $lokacije;
+    }
+
 
     public function brisanje($sifra)
     {
